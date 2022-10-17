aea2650ca0f651f64690ce150710c7d330b20d4b
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-10-17 12:14:25 -0600

    fix auto p10k theme install

--- terminal.sh
+++ terminal.sh
@@ -4,8 +4,9 @@
 sudo apt install -y zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 chsh -s $(which zsh)
-#echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 
 # Install fig
 curl -SsL https://pkg.fig.io/install.sh | bash

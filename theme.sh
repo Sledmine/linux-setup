71fc14bb4b378729f553e007cbe2a77149579722
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-10-16 23:36:40 -0600

    add theme setup

--- theme.sh
+++ theme.sh
@@ -1,6 +1,9 @@
 #/bin/bash
 
 # Install gnome theme
-# Icons: themes/Breeze-Noir-White-Blue.tar.gz
-# Cursor: themes/LyraB-cursors.tar.gz
-# Shell: Skeuos-Blue-Dark.tar.gz
+sudo tar -xvf themes/icons/Breeze-Noir-White-Blue.tar.gz -C /usr/share/icons
+sudo tar -xvf themes/cursor/LyraB-cursors.tar.gz -C /usr/share/icons
+sudo tar -xvf themes/shell/Skeuos-Blue-Dark.tar.gz -C /usr/share/themes
+
+# Set cursor size
+gsettings set org.gnome.desktop.interface cursor-size 32

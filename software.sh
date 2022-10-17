def1602c8f657d68c163973579eeb91b5df048c6
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-10-17 12:25:44 -0600

    fix get latest noisetorch

--- software.sh
+++ software.sh
@@ -12,10 +12,11 @@ rm google-chrome-stable_current_amd64.deb
 
 # Install Noise Torch (open source noise reduction)
 curl -s https://api.github.com/repos/noisetorch/NoiseTorch/releases/latest \
-| grep "browser_download_url.*NoiseTorch_x64" \
+| grep "browser_download_url.*.tgz\"" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| xargs wget
-tar -C $HOME -h -xzf NoiseTorch_x64_v0.12.2.tgz
+| xargs wget -O noisetorch.tgz
+tar -C $HOME -h -xzf noisetorch.tgz
+rm noisetorch.tgz
 # Allow noisetorch to capture system resources
 sudo setcap 'CAP_SYS_RESOURCE=+ep' ~/.local/bin/noisetorch
